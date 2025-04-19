60bc01a6dec9fdbe85716bbc6018bdb3ddb89667
Eric Strihagen <ericstri@kth.se>
2025-04-19 10:44:16 +0200

    Add cost to spells init
    This commit add costs as a value for each spell.

--- src/spell.py
+++ src/spell.py
@@ -1,8 +1,9 @@
 class Spell:
-    def __init__(self, name, power, spell_type):
+    def __init__(self, name, power, spell_type, cost=0):
         self.name = name
         self.power = power
         self.spell_type = spell_type
+        self.cost = cost
 
     def __str__(self):
         return f"{self.name} ({self.spell_type}) - Power: {self.power}, Cost: {self.cost}"
