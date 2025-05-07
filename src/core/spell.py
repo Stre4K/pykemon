8261f1a3ec5fc329091a4d2ac316a7dd9e2ac53c
Stre4K <eric.strihagen@gmail.com>
2025-05-07 20:18:22 +0200

    Fix bug concerning player2 not being able to cast spells
    This commit fixes the bug concerning player2's ability to cast spells.
    This bug was fixed by changing how spell objects are being compared.

--- src/core/spell.py
+++ src/core/spell.py
@@ -7,3 +7,15 @@ class Spell:
 
     def __str__(self):
         return f"{self.name} ({self.spell_type}) - Power: {self.power}, Cost: {self.cost}"
+
+    def __eq__(self, other):
+        return (
+            isinstance(other, Spell) and
+            self.name == other.name and
+            self.power == other.power and
+            self.spell_type == other.spell_type and
+            self.cost == other.cost
+        )
+
+    def __hash__(self):
+        return hash((self.name, self.power, self.spell_type, self.cost))
