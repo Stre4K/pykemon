b0dcd2eaa9925500159fc77b167cb2afb4e577b1
Love Sunnerfors Girbo <wowgirbo@live.se>
2025-05-08 17:02:57 +0200

    Fix issue #26 Fix issue #28

--- src/server/server.py
+++ src/server/server.py
@@ -1,5 +1,7 @@
 import socket
 import pickle
+import time
+
 from src.utils import utils
 
 HOST = '0.0.0.0'
@@ -7,18 +9,18 @@ PORT = 65432
 
 def game_loop(conn, player1, player2):
     while player1.current_hp > 0 and player2.current_hp > 0:
-        print(f"\n{utils.BOLD}--- New Turn ---{utils.RESET}")
-        print(f"{player1.name}: {utils.render_hp_bar(player1.current_hp, player1.max_hp)}")
-        print(f"{player2.name}: {utils.render_hp_bar(player2.current_hp, player2.max_hp)}\n")
+        print(f"\n{utils.BOLD}              --- New Turn ---{utils.RESET}")
+        print(f"{utils.GRAY}{utils.ITALIC}(You){utils.RESET}      {utils.render_hp_bar(player1.current_hp, player1.max_hp)} {utils.BOLD}{player1.name}{utils.RESET}")
+        print(f"{utils.GRAY}{utils.ITALIC}(Opponent){utils.RESET} {utils.render_hp_bar(player2.current_hp, player2.max_hp)} {utils.BOLD}{player2.name}{utils.RESET}\n")
+        utils.print_each_char(f"{utils.GRAY}{utils.ITALIC}Your turn to choose a spell!{utils.RESET}")
 
-        print("Your turn to choose a spell!")
         spell1 = utils.choose_spell(player1)
 
         # Send signal to client it's their turn
         conn.sendall(b'YOUR_TURN')
 
         # Receive spell from client
-        print("\nWaiting for Player 2 to choose a spell...")
+        utils.print_each_char(f"{utils.GRAY}{utils.ITALIC}Waiting for opponent to chose a spell...\n{utils.RESET}")
         spell2 = pickle.loads(conn.recv(4096))
 
         # Determine turn order
@@ -27,37 +29,41 @@ def game_loop(conn, player1, player2):
         else:
             first, first_spell, second, second_spell = player2, spell2, player1, spell1
 
+        # Send updated attack order and attacks to client
+        conn.sendall(pickle.dumps({
+            'firstAttacker': first,
+            'secondAttacker': second,
+            'firstSpell': first_spell,
+            'secondSpell': second_spell
+        }))
+
         # First attack
-        print(f"\n{first.name} attacks first!")
-        first.cast_spell(first_spell, second)
-
-        if second.current_hp <= 0:
-            break
-
+        first.cast_spell(first_spell, second, True)
+        print("\n")
+        time.sleep(1)
         # Second attack
-        print(f"\n{second.name} attacks second!")
-        second.cast_spell(second_spell, first)
+        second.cast_spell(second_spell, first, True)
+        print("\n")
+        time.sleep(3)
 
         # Send updated states of both players to client
         conn.sendall(pickle.dumps({
-            'attacks': [
-                {'attacker': first.name, 'spell': first_spell.name, 'target': second.name, 'target_hp': second.current_hp},
-                {'attacker': second.name, 'spell': second_spell.name, 'target': first.name, 'target_hp': first.current_hp}
-            ],
             'player1': player1,
-            'player2': player2
+            'player2': player2,
         }))
 
     # Game over message
-    if player1.current_hp <= 0 and player2.current_hp <= 0:
-        result_msg = f"{utils.YELLOW}It's a draw!{utils.RESET}"
-    elif player2.current_hp <= 0:
-        result_msg = f"{utils.GREEN}You win!{utils.RESET}"
-    else:
-        result_msg = f"{utils.RED}You lose!{utils.RESET}"
-
-    print(result_msg)
     conn.sendall(b'GAMEOVER')
+    print(f"{utils.GRAY}{utils.ITALIC}(You){utils.RESET}      {utils.render_hp_bar(player1.current_hp, player1.max_hp)} {utils.BOLD}{player1.name}{utils.RESET}")
+    print(f"{utils.GRAY}{utils.ITALIC}(Opponent){utils.RESET} {utils.render_hp_bar(player2.current_hp, player2.max_hp)} {utils.BOLD}{player2.name}{utils.RESET}\n")
+    if player1.current_hp <= 0 and player2.current_hp <= 0:
+        result_msg = f"{utils.YELLOW}{utils.BOLD}It's a draw!{utils.RESET}"
+    elif player2.current_hp <= 0:
+        result_msg = f"{utils.GREEN}{utils.BOLD}You win!{utils.RESET}"
+    else:
+        result_msg = f"{utils.RED}{utils.BOLD}You lose!{utils.RESET}"
+
+    utils.print_each_char(result_msg)
 
 def server():
     # Set up Pykemon
@@ -68,7 +74,7 @@ def server():
     server.bind((HOST, PORT))
     server.listen()
     conn, addr = server.accept()
-    print(f"{utils.GREEN}Player 2 connected from {addr}{utils.RESET}")
+    print(f"{utils.GREEN}Player 2 connected from {addr}{utils.RESET}\n")
 
     # Player 1 picks Pokémon
     player1 = utils.choose_pokemon(pykemon_list)
@@ -80,10 +86,11 @@ def server():
     conn.sendall(pickle.dumps(player1))
 
     # Receive Pokémon from Player 2
+    utils.print_each_char(f"{utils.GRAY}{utils.ITALIC}Waiting for opponent to chose a pokemon...\n{utils.RESET}")
     player2 = pickle.loads(conn.recv(4096))
 
-    print(f"{utils.CYAN}Your opponent chose {player2.name}!{utils.RESET}")
-    print(player2.spells)
+    utils.print_each_char(f"{utils.CYAN}Your opponent chose {player2.name}!{utils.RESET}\n\n")
+    time.sleep(2)
 
     # Reset HP
     player1.reset()
